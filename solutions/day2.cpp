9e9bb2b5f8bbf105cc1c61cfb819d05be4b0548d
FurryBasket9978 <pgillbama@icloud.com>
2025-12-04 22:36:11 -0600

    finished day 2

--- solutions/day2.cpp
+++ solutions/day2.cpp
@@ -21,9 +21,33 @@ int main() {
         
         for (long long pos = stoll(start); pos <= stoll(end); pos++) {
             std::string num = std::to_string(pos);
-            long long midpoint = (num.length() / 2) - 1; 
-            if (num.length() % 2 == 0 && num.substr(0, num.length() - midpoint - 1) == num.substr(midpoint+1, num.length() - 1)) {
-                total += pos; 
+            long long midpoint = (num.length() / 2) - 1;
+            
+            //one digit numbers can't be counted
+            if (num.length() == 1) {
+                continue;
+            }
+
+            for (int length = 1; length <= num.length() - midpoint - 1; length++) {
+                //there cannot be an occurence if the length of the number is not divisible by the length of the substring we're trying to find 
+                if (num.length() % length != 0) {
+                    continue;
+                }
+                
+                int target = stoi(num.substr(0, length));
+                bool isValid = true;
+                for (int pos = length; pos < num.length(); pos += length) {
+                    if (stoi(num.substr(pos, length)) != target) {
+                        isValid = false; 
+                        break;
+                    } 
+                }
+
+                if (isValid) {
+                    total += stoll(num);
+                    //doesnt matter how many we find, just count it once
+                    break;
+                }
             }
         } 
 
