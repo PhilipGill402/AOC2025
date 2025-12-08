0e5775b430754a03a928d1d2abad3479bef2eff6
FurryBasket9978 <pgillbama@icloud.com>
2025-12-08 13:35:02 -0600

    finished up day 4 challenge 1

--- solutions/day4_1.cpp
+++ solutions/day4_1.cpp
@@ -1,6 +1,39 @@
 #include <fstream>
 #include <iostream>
 
+int max_rows = 0;
+int max_cols = 0;
+
+int count_neighbors(int arr[], int row, int col) {
+    int total = 0;
+    
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+             
+            int new_row = row + i;
+            int new_col = col + j;
+
+            if (new_row < 0 || new_row >= max_rows) {
+                continue;
+            } else if (new_col < 0 || new_col >= max_cols) {
+                continue;
+            }
+
+            int index = (new_row * max_cols) + new_col; 
+
+            if (arr[index] == 1) {
+                total++;
+            }
+        }
+    }
+
+    return total;
+}
+
 int main() {
     std::ifstream file("/Users/philipgill/Developer/c/AOC2025/inputs/day4.txt");
     int num_lines = std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n');
@@ -12,21 +45,43 @@ int main() {
     std::string line;
     getline(file, line);
     
-    std::cout << line << "\n"; 
-    int arr[num_lines][line.length()];
+    max_rows = num_lines;
+    max_cols = line.length();
+    int arr[max_cols * max_rows];
     
     int line_no = 0;
     do { 
         for (int i = 0; i < line.length(); i++) {
+            int index = (line_no * max_cols) + i;
             char curr_char = line[i];
 
             if (curr_char == '@') {
-                arr[line_no][i] = 1;
+                arr[index] = 1;
             } else {
-                arr[line_no][i] = 0;
+                arr[index] = 0;
             }
         }
 
         line_no++;
     } while (getline(file, line));
+    
+    int total = 0;
+
+    for (int row = 0; row < max_rows; row++) {
+        for (int col = 0; col < max_cols; col++) {
+            int index = (row * max_cols) + col;
+            int neighbors = 0;
+
+            if (arr[index] == 1) {
+
+                neighbors = count_neighbors(arr, row, col);
+                
+                if (neighbors < 4) {
+                    total++;
+                } 
+            }
+        }
+    }
+
+    std::cout << total << "\n";
 }
