ede4551547836688bdf64562fb6167e20a110402
FurryBasket9978 <pgillbama@icloud.com>
2025-12-12 10:35:49 -0600

    finished challenge 2 of day 5

--- solutions/day5_2.cpp
+++ solutions/day5_2.cpp
@@ -2,15 +2,19 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <set>
+
+typedef struct range_t {
+    long long start;
+    long long stop;
+} range_t;
 
 int main() {
     std::ifstream file("/Users/philipgill/Developer/c/AOC2025/inputs/day5.txt");
     
-    std::set<long long> ids;
-
     std::string line;
     getline(file, line);
+    std::vector<range_t> ranges;
+    long long total = 0;
     
     //parse ranges
     while (line != "") { 
@@ -18,21 +22,39 @@ int main() {
         std::string start_str;
         std::string end_str;
         getline(str_stream, start_str, '-');
-        getline(str_stream, end_str, '-');
+        getline(str_stream, end_str);
         
         long long start = stoll(start_str);
         long long stop = stoll(end_str);
 
+        range_t new_range;
+        new_range.start = start;
+        new_range.stop = stop;
+
+
         //consider using formula: total += (stop - start) + 1; (PROBLEM: counts numbers multiple times) 
+        
+        for (int i = 0; i < ranges.size(); ) {
+            range_t range = ranges[i];
+            
+            //if the new range and old range overlap or touch, then merge them
+            if (!(new_range.stop < range.start - 1 || range.stop < new_range.start - 1)) {
+                new_range.start = std::min(new_range.start, range.start);
+                new_range.stop = std::max(new_range.stop, range.stop);
 
-        for (long long i = start; i <= stop; i++) {
-            ids.insert(i);
-        } 
-
+                total -= (range.stop - range.start) + 1;
+                ranges.erase(ranges.begin() + i);
+            } else {
+                i++;
+            }
+        }    
+        
+        ranges.push_back(new_range);
+        total += (new_range.stop - new_range.start) + 1;
         getline(file, line);
     }
+    
 
 
-
-    std::cout << ids.size() << "\n";
+    std::cout << total << "\n";
 }
