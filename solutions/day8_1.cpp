ec461c06ada3a08d85d41d0fa197f16272099468
FurryBasket9978 <pgillbama@icloud.com>
2026-01-01 21:58:08 -0600

    finished day 8 challenge 1

--- solutions/day8_1.cpp
+++ solutions/day8_1.cpp
@@ -7,7 +7,6 @@ struct vec3 {
     int x;
     int y;
     int z;
-    vec3* next;
 
     bool operator==(const vec3& other) const {
         return x == other.x && y == other.y && z == other.z;
@@ -20,50 +19,46 @@ struct vec3 {
     } 
 };
 
-double distance(vec3 vec_a, vec3 vec_b) {
-    return sqrt((vec_a.x - vec_b.x)*(vec_a.x - vec_b.x) + (vec_a.y - vec_b.y)*(vec_a.y - vec_b.y) + (vec_a.z - vec_b.z)*(vec_a.z - vec_b.z));
+struct Edge {
+    vec3 box_a;
+    vec3 box_b;
+    long long distance;
+};
+
+long long distance(vec3 vec_a, vec3 vec_b) {
+    long long dx = (long long)vec_a.x - (long long)vec_b.x;
+    long long dy = (long long)vec_a.y - (long long)vec_b.y;
+    long long dz = (long long)vec_a.z - (long long)vec_b.z;
+    return dx*dx + dy*dy + dz*dz;
 }
 
+bool comparator(Edge edge_a, Edge edge_b) {
+    return edge_a.distance < edge_b.distance;
+}
 
-//O(N^2) consider sorting the vector by distance somehow and then finding the lowest distance that way
-std::vector<vec3> find_lowest_distance(std::vector<vec3> &boxes, std::vector<vec3> &seen) {
-    std::vector<vec3> lowest_distance = { vec3{ 0, 0, 0, nullptr }, vec3{ 0, 0, 0, nullptr } };
-    lowest_distance.shrink_to_fit();
-    int min_dst = -1;
+bool size_comparator(std::vector<vec3> circuit_a, std::vector<vec3> circuit_b) {
+    return circuit_a.size() > circuit_b.size();
+}
+
+std::vector<Edge> generate_edges(std::vector<vec3> boxes) {
+    std::vector<Edge> edges;
     for (int i = 0; i < boxes.size(); i++) {
-        vec3 box1 = boxes.at(i);
-        for (int j = 0; j < boxes.size(); j++) {
-            if (i == j) {
-                continue;
-            }
+        for (int j = i+1; j < boxes.size(); j++) {
+            edges.push_back({ boxes.at(i), boxes.at(j), distance(boxes.at(i), boxes.at(j)) });
+        }
+    } 
 
-            vec3 box2 = boxes.at(j);
-            double dst = distance(box1, box2);
-            if (dst < min_dst || min_dst == -1) {
-                
-                //need to make sure that these are not in seen
-                bool is_contained = false;
-                for (int i = 0; i < (int)seen.size() - 1; i += 2) {
-                    if ((seen[i] == box1 && seen[i+1] == box2) || (seen[i] == box2 && seen[i+1] == box1)) {
-                        is_contained = true;
-                        break;
-                    }
-                }
-                
-                if (!is_contained) {
-                    lowest_distance[0] = box1;
-                    lowest_distance[1] = box2;
-                    min_dst = dst;
-                }
-                
-            } 
+    return edges;
+}
+
+int get_circuit_index(std::vector<std::vector<vec3>>& circuits, vec3 box) {
+    for (int i = 0; i < circuits.size(); i++) {
+        if (find(circuits.at(i).begin(), circuits.at(i).end(), box) != circuits.at(i).end()) {
+            return i;
         }
     }
-    
-    seen.push_back(lowest_distance[0]);
-    seen.push_back(lowest_distance[1]);
 
-    return lowest_distance;
+    return -1;
 }
 
 int main() {
@@ -81,27 +76,75 @@ int main() {
         getline(line_stream, y, ',');
         getline(line_stream, z, ',');
         
-        pos = { stoi(x), stoi(y), stoi(z), nullptr };
+        pos = { stoi(x), stoi(y), stoi(z) };
         boxes.push_back(pos);
     }
     
-    std::vector<vec3> heads;
-    std::vector<vec3> seen;
-    for (int i = 0; i < 10; i++) {
-        std::vector<vec3> lowest = find_lowest_distance(boxes, seen);
-        std::cout << lowest[0].to_string() << "\n" << lowest[1].to_string() << "\n";
+    std::vector<Edge> edges = generate_edges(boxes);
+    sort(edges.begin(), edges.end(), comparator);
+
+    std::vector<std::vector<vec3>> circuits;
+     
+    for (int i = 0; i < 1000; i++) {
+        Edge edge = edges.at(i);
+
+        int index_a = get_circuit_index(circuits, edge.box_a);
+        int index_b = get_circuit_index(circuits, edge.box_b);
         
-        //if there is no vector in the circuit then make the next vector in the circuit the other lowest vector and add it to the list of circuits
-        if (lowest[0].next == nullptr) {
-            lowest[0].next = &lowest[1];
-            heads.push_back(lowest[0]);
-        }
-        
-        //if there are already vectors in the circuit then go until a vector does not have a next and then add the vector to the end of the list
-        vec3 tail = lowest[0];
-        while (tail.next != nullptr) {
-            tail = *tail.next; 
+        if (index_a == -1 && index_b == -1) {
+            //no circuits contain either box 
+            circuits.push_back({ edge.box_a, edge.box_b });
+        } else if (index_a != -1 && index_b == -1) {
+            //box a is in a circuit
+            if (find(circuits.at(index_a).begin(), circuits.at(index_a).end(), edge.box_b) == circuits.at(index_a).end()) {
+                circuits.at(index_a).push_back(edge.box_b);
+            }
+        } else if (index_a == -1 && index_b != -1) {
+            //box b is in a circuit
+            if (find(circuits.at(index_b).begin(), circuits.at(index_b).end(), edge.box_a) == circuits.at(index_b).end()) {
+                circuits.at(index_b).push_back(edge.box_a);
+            }
+        } else {
+
+            //both are in the same circuit
+            if (index_a == index_b) {
+                continue;
+            }
+            
+
+            if (index_a > index_b) {
+                std::swap(index_a, index_b);
+            } 
+
+            //if not then merge circuits
+            for (auto& box : circuits.at(index_b)) {
+                if (find(circuits.at(index_a).begin(), circuits.at(index_a).end(), box) == circuits.at(index_a).end()) {
+                    circuits.at(index_a).push_back(box);
+                }
+            }
+
+            circuits.erase(circuits.begin() + index_b);
         }
     }
     
+    sort(circuits.begin(), circuits.end(), size_comparator); 
+    
+    /*
+    for (std::vector<vec3> circuit : circuits) {
+        for (vec3 box : circuit) {
+            std::cout << box.to_string() << "\n";
+        }
+
+        std::cout << "\n";
+    }
+    */
+    
+    std::cout << circuits.at(0).size() << "\n";
+    std::cout << circuits.at(1).size() << "\n";
+    std::cout << circuits.at(2).size() << "\n";
+
+
+    long long result = circuits.at(0).size() * circuits.at(1).size() * circuits.at(2).size();
+
+    std::cout << result << "\n";
 }
